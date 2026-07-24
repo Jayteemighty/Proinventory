72c58ae101ee01edf046e343b1056fb9627acf90
Joshua Adesina <140406481+Jayteemighty@users.noreply.github.com>
2026-07-24 14:26:09 +0000

    initialize base model

--- backend/app/db/database.py
+++ backend/app/db/database.py
@@ -2,6 +2,16 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 from app.config.settings import settings
+from sqlalchemy.orm import Session
+
+
+def get_db():
+    db: Session = SessionLocal()
+
+    try:
+        yield db
+    finally:
+        db.close()
 
 engine = create_engine(
     settings.DATABASE_URL,
