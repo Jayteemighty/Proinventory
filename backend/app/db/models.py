1732bd4d833833e6ab88e1478d26c4c6f7e4cbb0
Joshua Adesina <140406481+Jayteemighty@users.noreply.github.com>
2026-08-15 11:04:53 +0000

    register new models to alembic

--- backend/app/db/models.py
+++ backend/app/db/models.py
@@ -1,7 +1,13 @@
 from app.organization.models import Organization
 from app.department.models import Department
+from app.permissions.models import Permission
+from app.roles.models import Role
+from app.roles.role_permission import RolePermission
 
 __all__ = [
     "Organization",
     "Department",
+    "Permission",
+    "Role",
+    "RolePermission",
 ]
