8a89eb6fd1978cb61442280a9d911c82ca94f6c0
Joshua Adesina <140406481+Jayteemighty@users.noreply.github.com>
2026-08-15 10:48:52 +0000

    create role permissions

--- backend/app/roles/role_permission.py
+++ backend/app/roles/role_permission.py
@@ -1,20 +1,18 @@
 from sqlalchemy import ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column
 
-from app.db.base import AppBaseModel
+from app.db.base import Base
 
 
-class RolePermission(AppBaseModel):
+class RolePermission(Base):
     __tablename__ = "role_permission"
 
     role_id: Mapped[str] = mapped_column(
         ForeignKey("role.id", ondelete="CASCADE"),
-        nullable=False,
-        index=True,
+        primary_key=True,
     )
 
     permission_id: Mapped[str] = mapped_column(
         ForeignKey("permission.id", ondelete="CASCADE"),
-        nullable=False,
-        index=True,
+        primary_key=True,
     )
