14c17050c893e5853fc5b0f931a37a68b4d45ab0
Joshua Adesina <140406481+Jayteemighty@users.noreply.github.com>
2026-08-23 18:24:14 +0000

    update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,10 +1,15 @@
 from fastapi import FastAPI
+from app.auth.router import router as auth_router
 from app.config.settings import settings
+
 
 app = FastAPI(
     title=settings.APP_NAME,
     version=settings.APP_VERSION,
 )
+
+
+app.include_router(auth_router)
 
 
 @app.get("/")
